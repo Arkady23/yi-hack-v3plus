56157378e5be5313cc3e47c5961ed7cb0d30b16f
Arkady23 <79393209+Arkady23@users.noreply.github.com>
2021-02-27 01:46:11 +1000

    Update system.sh

--- Russian/script/system.sh
+++ Russian/script/system.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-CONF_FILE="/yi-hack-v3/etc/system.conf"
-
 if [ -d "/usr/yi-hack-v3" ]; then
-        YI_HACK_V3_PREFIX="/usr"
+        YI_HACK_V3_PREFIX="/usr/yi-hack-v3"
 elif [ -d "/home/yi-hack-v3" ]; then
-        YI_HACK_V3_PREFIX="/home"
+        YI_HACK_V3_PREFIX="/home/yi-hack-v3"
 fi
 
 get_config(){
-        grep $1 $YI_HACK_V3_PREFIX$CONF_FILE | cut -d "=" -f2
+	grep $1 $YI_HACK_V3_PREFIX/etc/system.conf | cut -d "=" -f2
 }
 
 if [ -d "/usr/yi-hack-v3" ]; then
@@ -21,14 +19,12 @@ elif [ -d "/home/yi-hack-v3" ]; then
 fi
 
 
-HOST = $(cat $YI_HACK_V3_PREFIX/yi-hack-v3/etc/hostname)
-
-if [ -n "$HOST" ] ; then
-  hostname -F $HOST
+if [ -n "$(cat $YI_HACK_V3_PREFIX/etc/hostname)" ] ; then
+	hostname $(cat $YI_HACK_V3_PREFIX/etc/hostname)
 fi
 
 if [[ $(get_config HTTPD) == "yes" ]] ; then
-	httpd -p 80 -h $YI_HACK_V3_PREFIX/yi-hack-v3/www/
+	httpd -p 80 -h $YI_HACK_V3_PREFIX/www/
 fi
 
 if [[ $(get_config TELNETD) == "yes" ]] ; then
