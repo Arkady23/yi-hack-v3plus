96172a1767afaec21c6a40e6bc857a7fd7363382
Arkady23 <79393209+Arkady23@users.noreply.github.com>
2021-02-28 06:31:34 +1000

    Update startup.sh

--- Russian/startup.sh
+++ Russian/startup.sh
@@ -5,25 +5,21 @@
  elif [ -f "/home/hd1/yi-hack-v3/startup.sh" ]; then
 	pr="/home/hd1"
  fi
+ if [ -d "/usr/yi-hack-v3" ]; then
+        to="/usr/yi-hack-v3"
+ elif [ -d "/home/yi-hack-v3" ]; then
+        to="/home/yi-hack-v3"
+ fi
  fr="$pr/yi-hack-v3"
- to="/home/yi-hack-v3"
  ta="/home/app"
 
+ sed -i '/.\/rmm /,$ d' $ta/init.sh
+ cat $ta/init.sh $fr/_init.sh > $ta/_init.sh
+ mv -f $ta/_init.sh $ta/init.sh
+ chmod 0755 $ta/init.sh
 
- if [ -d "/usr/yi-hack-v3" ]; then
-        YI_HACK_PREFIX="/usr/yi-hack-v3"
- elif [ -d "/home/yi-hack-v3" ]; then
-        YI_HACK_PREFIX="/home/yi-hack-v3"
- fi
- CAMERA_MODEL=$(sed -n 1p $YI_HACK_PREFIX/.hackinfo | sed -n '/.*=/s///p')
- if [ $CAMERA_MODEL == "Yi Outdoor" ]; then
-	cp -f $fr/app/init_Yi_Outdoor.sh $ta/init.sh
- else
-	cp -f $fr/app/init_Yi_Home_17CN.sh $ta/init.sh
- fi
  cp -f $fr/app/vencrtsp_v2 $ta/
  chmod 0755 $ta/vencrtsp_v2
- chmod 0755 $ta/init.sh
 
  cp -fr $fr/bin/ $to/
  chmod 0755 $to/bin/*
@@ -36,7 +32,7 @@
  rm -fr $to/www/css/
  rm -fr $to/www/js/
  rm -f $to/www/*
- ln -s $pr/record /home/yi-hack-v3/www/record
+ ln -s $pr/record $to/www/record
  cp -fr $fr/www/ $to/
  chmod 0755 $to/www/cgi-bin/*
 
@@ -48,6 +44,8 @@
 
  rm -f $to/bin/dropbearmulti
  rm -f $to/bin/lwsws
+ rm -f $to/lib/libwebsockets.so
+ rm -f $to/lib/libuv.so.1.0.0
  rm -fr $fr/
  sync
  reboot
