a51035c45b2221b3d82a6bc16a3d05aa0b330400
Arkady23 <79393209+Arkady23@users.noreply.github.com>
2021-03-02 22:17:20 +1000

    Update startup.sh

--- Russian/startup.sh
+++ Russian/startup.sh
@@ -6,20 +6,34 @@
 	pr="/home/hd1"
  fi
  if [ -d "/usr/yi-hack-v3" ]; then
-        to="/usr/yi-hack-v3"
+	to="/usr/yi-hack-v3"
  elif [ -d "/home/yi-hack-v3" ]; then
-        to="/home/yi-hack-v3"
+	to="/home/yi-hack-v3"
  fi
  fr="$pr/yi-hack-v3"
  ta="/home/app"
+ ven="vencrtsp_v2"
+ cm=$(sed -n 1p $to/.hackinfo | sed -n '/.*=/s///p')
+
+ case $cm in
+   *"17CN"*)
+	rs=$ven
+	;;
+   *)
+	rs="rtsp2303"
+	sed -i "s/$ven/$rs/" $fr/app/_init.sh
+	cp -f $fr/app/libstdc++.so.6  /home/lib/
+	chmod 0755 /home/lib/libstdc++.so.6
+	;;
+ esac
 
  sed -i '/.\/rmm /,$ d' $ta/init.sh
- cat $ta/init.sh $fr/_init.sh > $ta/_init.sh
+ cat $ta/init.sh $fr/app/_init.sh > $ta/_init.sh
  mv -f $ta/_init.sh $ta/init.sh
  chmod 0755 $ta/init.sh
 
- cp -f $fr/app/vencrtsp_v2 $ta/
- chmod 0755 $ta/vencrtsp_v2
+ cp -f $fr/app/$rs $ta/
+ chmod 0755 $ta/$rs
 
  cp -fr $fr/bin/ $to/
  chmod 0755 $to/bin/*
@@ -48,4 +62,4 @@
  rm -f $to/lib/libuv.so.1.0.0
  rm -fr $fr/
  sync
- reboot
+reboot
