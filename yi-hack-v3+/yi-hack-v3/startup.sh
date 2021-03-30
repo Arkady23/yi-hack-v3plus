86e02311bddc60fc66be1f6c4587c76f9eaffd79
Arkady23 <a@kornienko.ru>
2021-03-30 22:56:45 +1000

    Update startup.sh

--- yi-hack-v3+/yi-hack-v3/startup.sh
+++ yi-hack-v3+/yi-hack-v3/startup.sh
@@ -13,10 +13,14 @@
  fr="$pr/yi-hack-v3"
  ta="/home/app"
 
+ echo "Begin install. from=$fr to=$to" > $pr/install.log
+
  sed -i '/.\/rmm /,$ d' $ta/init.sh
  cat $ta/init.sh $fr/app/_init.sh > $ta/_init.sh
  mv -f $ta/_init.sh $ta/init.sh
  chmod 0755 $ta/init.sh
+
+ echo "1. $ta/init.sh ready." >> $pr/install.log
 
  rm -f $to/bin/lwsws
  rm -f $to/bin/dropbearmulti
@@ -24,6 +28,8 @@
  rm -fr $to/etc/dropbear/
  rm -f $to/etc/dropbear/
  cp -fr $fr/etc/ $to/
+
+ echo "2. $to/etc/ ready." >> $pr/install.log
 
  rm -fr $to/www/css/
  rm -fr $to/www/js/
@@ -33,6 +39,8 @@
  rm -f $to/lib/libuv.so.1
  rm -f $to/lib/libuv.so.1.0.0
 
+ echo "3. $to/www/ and lib/ clear." >> $pr/install.log
+
  cp -f $fr/app/rRTSPServer $ta/
  cp -f $fr/app/h264grabber $ta/
  cp -f $fr/app/libstdc++.so.6  /home/lib/
@@ -40,12 +48,18 @@
  chmod 0755 $ta/h264grabber
  chmod 0755 /home/lib/libstdc++.so.6
 
+ echo "4. $ta/ ready." >> $pr/install.log
+
  cp -fr $fr/bin/ $to/
  chmod 0755 $to/bin/*
+
+ echo "5. $to/bin/ ready." >> $pr/install.log
 
  ln -s $pr/record $to/www/record
  cp -fr $fr/www/ $to/
  chmod 0755 $to/www/cgi-bin/*
+
+ echo "6. $to/www/ ready." >> $pr/install.log
 
  if [ ! -f "/etc/TZ" ]; then
 	cp -f $fr/script/TZ /etc/
@@ -55,11 +69,21 @@
  chmod 0755 $to/script/system.sh
  chmod 0755 $to/script/clean_records.sh
 
+ echo "7. $to/script/ ready." >> $pr/install.log
+
  if [[ ! -s $to/etc/crontabs/root ]]; then
 	mkdir -p $to/etc/crontabs
 	echo "0 * * * * $to/script/clean_records.sh 2" >> $to/etc/crontabs/root
  fi
 
+ echo "8. $to/etc/crontabs/ ready." >> $pr/install.log
+
  rm -fr $fr/
+
+ echo "9. This install folder deleted." >> $pr/install.log
+
  sync
+
+ echo "10. Rebooting..." >> $pr/install.log
+
 reboot
